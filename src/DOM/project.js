ce8c00d4897613a7aeabef1ee4db5d0b55de1e09
paul <ajuzepaul@gmail.com>
2020-10-07 01:17:07 +0300

    Add default Project

---
+++ src/DOM/project.js
@@ -0,0 +1,18 @@
+const projectDom = () => {
+/*	const container = document.createElement('div');
+	const newProjectButton = document.createElement('button');
+	const formEle = document.createElement('form');
+	const titleEle = document.createElement('input');
+	const descriptEle = document.createElement('textarea');
+
+	newProjectButton.textContent = 'Add New Project';
+	titleEle.setAttribute('type', 'text');
+	titleEle.setAttribute('placeholder', 'TITLE');
+
+	container.appendChild(newProjectButton);
+
+	document.body.appendChild(container);
+	document.body.appendChild(formEle);*/
+};
+
+export default projectDom
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Project from './utils/project'
+import ProjectDom from './DOM/project'
 
-const newP = new Project('sdfsd', 'hdfisudhf');
-console.log(newP.todos)
+const defaultProject = new Project('Default', 'This project contains your default todo list. You can add new projects with the add button');
+
+ProjectDom();
