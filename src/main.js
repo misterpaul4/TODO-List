0cdf41d4e287feb9fc861ce92c33556e4c9e3dc8
paul <ajuzepaul@gmail.com>
2020-10-09 01:20:16 +0300

    Build header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Project from './utils/project'
 import ProjectDom from './DOM/project'
+import header from './components/header/header'
 
 const defaultProject = new Project('Default', 'This project contains your default todo list. You can add new projects with the add button');
-
 ProjectDom();
+
+header();
