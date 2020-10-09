2e19c431679fbc2593777c10d0439ae931fab496
paul <ajuzepaul@gmail.com>
2020-10-09 18:47:22 +0300

    Add contents to the projects on the sidebar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import './main.scss';
 import renderHeader from './components/header/header';
 import renderSideBar from './components/sidebar/sidebar';
+import renderProject from './components/project/project';
+import Project from './utils/project';
 
 renderHeader();
 renderSideBar();
+
+
+// testing
+let projects = []
+const defaultProject = new Project('shopping', 'shopping list');
+const grocery = new Project('grocery', 'grocery list');
+projects.push(defaultProject);
+projects.push(grocery);
+renderProject(projects)
--- src/utils/project.js
+++ src/utils/project.js
@@ -1,7 +1,8 @@
 export default class Project {
-	constructor(title, description) {
+	constructor(title, description, color) {
 		this.title = title;
 		this.description = description;
+		this.color = color;
 		this.todos = [3,4,5];
 	}
 }
