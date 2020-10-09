16742dd63c8cde739dde33225e2f3d2222ea60fd
paul <ajuzepaul@gmail.com>
2020-10-09 22:43:54 +0300

    Build new project using DOM

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,26 +5,51 @@ import Project from '../../utils/project';
 
 function renderForm(projects) {
   const formContainer = document.createElement('div');
+  const form = document.createElement('form');
+  const header = document.createElement('h2');
+  const titleContainer = document.createElement('div');
+  const titleLabel = document.createElement('label');
+  const titleInput = document.createElement('input');
+  const descriptionContainer = document.createElement('div');
+  const descriptionLabel = document.createElement('label');
+  const descriptionInput = document.createElement('textarea');
+  const submitBtn = document.createElement('button');
 
   formContainer.id = 'project-form';
+  titleInput.id = 'title';
+  descriptionInput.id = 'description';
+  submitBtn.id = 'project-submit';
+
   formContainer.className = 'form-container';
+  titleContainer.className = 'form-group';
+  titleInput.className = 'form-control';
+  descriptionContainer.className = 'form-group';
+  descriptionInput.className = 'form-control';
+  submitBtn.classList.add('btn', 'btn-primary');
 
-  formContainer.innerHTML = `
-    <form>
-      <h2>New Project</h2>
-      <div class="form-group">
-        <label for="title">Title</label>
-        <input type="text" name="title" class="form-control" id="title">
-      </div>
+  titleLabel.setAttribute('for', 'title');
+  titleInput.setAttribute('type', 'text');
+  titleInput.setAttribute('name', 'title');
+  descriptionLabel.setAttribute('for', 'description');
+  descriptionInput.setAttribute('name', 'description');
+  descriptionInput.setAttribute('rows', '5');
+  submitBtn.setAttribute('type', 'submit');
 
-      <div class="form-group">
-        <label for="description">Description</label>
-        <textarea class="form-control" name="description" id="description" rows="5"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary" id="project-submit">Submit</button>
-    </form>
-  `;
 
+  titleLabel.textContent = 'Title';
+  descriptionLabel.textContent = 'Description';
+  submitBtn.textContent = 'Submit';
+  header.textContent = 'New Project';
+
+  formContainer.appendChild(form);
+  form.appendChild(header);
+  form.appendChild(titleContainer);
+  titleContainer.appendChild(titleLabel);
+  titleContainer.appendChild(titleInput);
+  form.appendChild(descriptionContainer);
+  descriptionContainer.appendChild(descriptionLabel);
+  descriptionContainer.appendChild(descriptionInput);
+  form.appendChild(submitBtn);
   document.body.appendChild(formContainer);
 }
 
