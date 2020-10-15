dc7e64135f264f64525153e2f3e9475e83b2d4af
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-10-15 15:28:40 +0100

    Initial build subtree commit

--- src/utils/todo.js
+++
@@ -1,9 +0,0 @@
-export default class Todo {
-  constructor(title, description, dueDate, priority, done = false) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.complete = done;
-  }
-}
